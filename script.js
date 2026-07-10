a2bfe0cc2165ed5f89dea8c0aed485853a05a737
aashish91422062-bit <aashish91422062@gmail.com>
2026-07-10 10:11:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
+/* ==================================
+   AshCore AI V1.0
+   script.js Final
+================================== */
 
+const menuBtn = document.getElementById("menuBtn");
+const closeMenu = document.getElementById("closeMenu");
+const sideMenu = document.getElementById("sideMenu");
+const overlay = document.getElementById("overlay");
+
+// Open Menu
+menuBtn.addEventListener("click", () => {
+    sideMenu.classList.add("active");
+    overlay.classList.add("active");
+});
+
+// Close Menu Button
+closeMenu.addEventListener("click", closeSidebar);
+
+// Close Overlay
+overlay.addEventListener("click", closeSidebar);
+
+function closeSidebar() {
+    sideMenu.classList.remove("active");
+    overlay.classList.remove("active");
+}
+
+// Close menu after clicking any link
+document.querySelectorAll(".side-menu a").forEach(link => {
+    link.addEventListener("click", () => {
+        closeSidebar();
+    });
+});
+
+// Welcome Message
+window.addEventListener("load", () => {
+    console.log("✅ AshCore AI Loaded Successfully");
+});
+
+// Future Notification
+document.querySelectorAll(".coming-badge").forEach(item => {
+
+    item.addEventListener("click", () => {
+
+        alert("🚀 This feature will be available in the next update.");
+
+    });
+
+});
